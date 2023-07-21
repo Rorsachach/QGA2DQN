2307206a1fbe3aae5e1eeeb2d0b178021f817d3d
Rorschach <40058257+Rorsachach@users.noreply.github.com>
2023-07-21 14:54:28 +0800

    commit

--- interdependent network/REINFORCE.py
+++ interdependent network/REINFORCE.py
@@ -12,7 +12,6 @@ class PolicyNet(torch.nn.Module):
         super(PolicyNet, self).__init__()
         self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
         self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
-        # self.stds = torch.nn.Parameter(torch.zeros(action_dim))
 
     def forward(self, x):
         """
@@ -21,7 +20,7 @@ class PolicyNet(torch.nn.Module):
         """
         x = F.relu(self.fc1(x))
         return torch.tanh(self.fc2(x))
-        # return F.softmax(self.fc2(x), dim=1)  # TODO: 1 2
+
 
 class Normal(torch.nn.Module):
     def __init__(self, action_dim):
@@ -38,47 +37,15 @@ class REINFORCE:
         self.policy_net = PolicyNet(state_dim, hidden_dim,
                                     action_dim).to(device)
         self.normal = Normal(action_dim)
-        self.optimizer = torch.optim.Adam(self.policy_net.parameters(),
-                                          lr=learning_rate)  # 使用Adam优化器
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=learning_rate)  # 使用Adam优化器
         self.gamma = gamma  # 折扣因子
         self.device = device
 
     def take_action(self, state):
         state = torch.tensor(state, dtype=torch.float).to(self.device)
         action_dist = self.normal(self.policy_net(state))  # 通过 state 生成概率密度函数
-        # probs = self.policy_net(state)
-        # action_dist = torch.distributions.Normal(
-        #     loc=probs,
-        #     scale=torch.nn.Parameter(torch.zeros(state.shape[0])).exp() * 0.05 * math.pi
-        # )
         action = action_dist.sample()  # 按照概率密度函数生成 state 对应的 action
         return action.numpy()
-
-    # def take_action(self, state):  # 根据动作概率分布随机采样
-    #     state = torch.tensor(state, dtype=torch.float).to(self.device)
-    #     probs = self.policy_net(state)
-    #     action_dist = torch.distributions.Categorical(probs)
-    #     action = action_dist.sample()
-    #     return action.numpy()  # TODO:
-
-    # def update(self, transition_dict):
-    #     reward_list = transition_dict['rewards']
-    #     state_list = transition_dict['states']
-    #     action_list = transition_dict['actions']
-    #
-    #     G = 0
-    #     self.optimizer.zero_grad()
-    #     for i in reversed(range(len(reward_list))):  # 从最后一步算起
-    #         reward = reward_list[i]
-    #         state = torch.tensor([state_list[i]],
-    #                              dtype=torch.float).to(self.device)
-    #         action = torch.tensor([action_list[i]]).view(-1, 1).to(self.device)
-    #         # log_prob = torch.log(self.policy_net(state).gather(1, action))  #TODO:
-    #         log_prob = torch.log(self.policy_net(state).squeeze(0).gather(1, action))
-    #         G = self.gamma * G + reward
-    #         loss = (-log_prob * G).sum()  # 每一步的损失函数
-    #         loss.backward()  # 反向传播计算梯度
-    #     self.optimizer.step()  # 梯度下降
 
     def update(self, transition_dict):
         reward_list = transition_dict['rewards']
